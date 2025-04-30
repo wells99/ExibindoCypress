ee3d709e3a7d10f9c993e1202273d01c1e7032e3
unknown <wellingtonsilva0678@digitalcollege.internal>
2025-04-30 08:49:19 -0300

    First commit

---
+++ cypress/e2e/spec.cy.js
@@ -0,0 +1,22 @@
+describe('template spec', () => {
+  it('passes', () => {
+    cy.visit('https://youtube.com');
+    cy.wait(3000);
+    cy.get('.ytSearchboxComponentInput')
+    .click()
+    .type("Ok google - Agora vai");
+
+    cy.get('.ytSearchboxComponentInput').type('{enter}')
+
+    cy.wait(3000);
+
+    cy.get('.ytSearchboxComponentClearButton > yt-touch-feedback-shape > .yt-spec-touch-feedback-shape > .yt-spec-touch-feedback-shape__fill')
+    .click()
+    
+    cy.get('.ytSearchboxComponentInput')
+    .click()
+    .type("Novo Teste");
+    cy.wait(3000)
+    cy.get('.ytSearchboxComponentSearchButton > yt-icon > .yt-icon-shape > div').click()
+  })
+})
